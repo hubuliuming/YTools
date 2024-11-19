5bb35443e3ffd909453a52df4357cbe9e2df4f73
Y <hubuliuming@gmail.com>
2024-11-19 22:57:26 +0800

    导入一些资源

--- Assets/Test/Scripts/Test1.cs
+++ Assets/Test/Scripts/Test1.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Sockets;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,30 +13,53 @@ using Random = UnityEngine.Random;
 public class Test1 : YMonoBehaviour
 {
     
+
+    
     private void Start()
     {
-        Random.InitState(4);
-        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        string filePath = @"C:\Users\91611\Documents\WeChat Files\wxid_rz1e49017c0a22\FileStorage\File\2024-10\CurveData.dat";
+        int floatsPerFrame = 5000;
 
-        socket.Poll(1000, SelectMode.SelectRead);
+        List<float[]> frames = ParseBinaryFile(filePath, floatsPerFrame);
+
+        // 示例输出：打印每帧的第一个浮点数
+        for (int i = 0; i < frames.Count; i++)
+        {
+            Debug.Log($"Frame {i + 1}, first float: {frames[i][0]}");
+        }
+    }
+    
+    static List<float[]> ParseBinaryFile(string filePath, int floatsPerFrame)
+    {
+        List<float[]> frames = new List<float[]>();
+
+        using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        using (BinaryReader br = new BinaryReader(fs))
+        {
+            while (br.BaseStream.Position < br.BaseStream.Length)
+            {
+                float[] frame = new float[floatsPerFrame];
+                for (int i = 0; i < floatsPerFrame; i++)
+                {
+                    if (br.BaseStream.Position < br.BaseStream.Length)
+                    {
+                        frame[i] = br.ReadSingle();
+                    }
+                    else
+                    {
+                        break; // 防止最后一个帧不完整的情况
+                    }
+                }
+                frames.Add(frame);
+            }
+        }
+
+        return frames;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log(Random.value);
-        }
-
-        var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
-
-        var moment = new Vector3(x, 0, y);
-
-        moment = Vector3.ClampMagnitude(moment, 5);
-        
-        moment = transform.TransformDirection(moment);
-        
+       
     }
 }
 
